cc9e2943a50ba8cdf6a320be24b1af2d33efac9b
SOOHYUNLIM <sh_7682@naver.com>
2020-06-03 22:24:02 +0900

    기본 틀 완성

--- src/components/FileUpload.vue
+++ src/components/FileUpload.vue
@@ -11,18 +11,37 @@
                 </v-radio-group>
             </v-col>
         </v-row>
-        <v-file-input accept="application/pdf" label="파일을 선택해주세요..." multiple></v-file-input>
-        <v-btn depressed block>확인</v-btn>
+        <v-file-input v-model="file" accept="application/pdf" label="파일을 선택해주세요..."></v-file-input>
+        <v-btn @click="fileUpload" depressed block>확인</v-btn>
     </v-layout>
 </template>
 
 <script>
+import Axios from "axios";
+
 export default {
     name: 'FileUpload'
 
     //확인을 누르면 axios호출
     //대기중으로 변경시키기 위해 상태 data 들고있기
     //누르면 상위 컴포넌트에게 대기하라 명령하고 응답오면 대기종료 보내기
+    ,
+    data() {
+      return {
+          file: null
+      }
+    },
+    methods: {
+        fileUpload() {
+            if(this.file) {
+                let formData = new FormData()
+                formData.append("file", this.file)
+                Axios.post("http://localhost:8080/ebook/upload", formData).then(()=>alert("파일 변환 완료!")).catch(error=>console.log(error))
+            } else {
+                alert("파일을 선택해주세요!")
+            }
+        }
+    }
 }
 </script>
 
--- src/views/EBookView.vue
+++ src/views/EBookView.vue
@@ -1,72 +1,78 @@
 <template>
-  <v-container class="container" fill-height fluid>
-      <v-row align-content justify="center">
-        <v-card width="50%" min-width="300">
-            <v-card-title>
-                <v-row>
-                    <v-col>
-                        <h1>Book Player</h1>
-                    </v-col>
-                    <v-col class="text-right">
-                        <v-btn @click="modal=true" icon color="blue">
-                            <v-icon>mdi-upload</v-icon>
-                        </v-btn>
-                    </v-col>
-                </v-row>
-            </v-card-title>
-            <v-card-text>
-                <v-audio style="box-shadow:none"></v-audio>
-                <v-list dense>
-                    <v-subheader>
-                        <v-flex xs9>Title</v-flex><v-flex xs1>Play</v-flex><v-flex xs1>Down</v-flex><v-flex xs1>Del</v-flex>
-                    </v-subheader>
-                    <v-list-item-group v-model="item" color="primary">
-                        <v-list-item v-for="(item, i) in items" :key="i">
-                            <v-list-item-content>
-                                <v-list-item-title v-text="item.text"></v-list-item-title>
-                            </v-list-item-content>
-                            <v-list-item-icon>
-                                <v-icon>mdi-play</v-icon>
-                                <v-icon>mdi-download</v-icon>
-                                <v-icon>mdi-delete</v-icon>
-                            </v-list-item-icon>
-                        </v-list-item>
-                    </v-list-item-group>
-                </v-list>
-                <v-pagination :length="4" v-model="page" circle></v-pagination>
-            </v-card-text>
-        </v-card>
-      </v-row>
-      <FileUploadModal :modal="modal"></FileUploadModal>
-  </v-container>
+    <v-container class="container" fill-height fluid>
+        <v-row align-content justify="center">
+            <v-scroll-x-transition>
+                <v-card v-show="true" width="50%" min-width="300">
+                    <v-card-title>
+                        <v-row>
+                            <v-col>
+                                <h1>Book Player</h1>
+                            </v-col>
+                            <v-col class="text-right">
+                                <v-btn @click="modal=true" icon color="blue">
+                                    <v-icon>mdi-upload</v-icon>
+                                </v-btn>
+                            </v-col>
+                        </v-row>
+                    </v-card-title>
+                    <v-card-text>
+                        <v-audio style="box-shadow:none"></v-audio>
+                        <v-list dense>
+                            <v-subheader>
+                                <v-flex xs9>Title</v-flex>
+                                <v-flex xs1>Play</v-flex>
+                                <v-flex xs1>Down</v-flex>
+                                <v-flex xs1>Del</v-flex>
+                            </v-subheader>
+                            <v-list-item-group v-model="item" color="primary">
+                                <v-list-item v-for="(item, i) in items" :key="i">
+                                    <v-list-item-content>
+                                        <v-list-item-title v-text="item.text"></v-list-item-title>
+                                    </v-list-item-content>
+                                    <v-list-item-icon>
+                                        <v-icon>mdi-play</v-icon>
+                                        <v-icon>mdi-download</v-icon>
+                                        <v-icon>mdi-delete</v-icon>
+                                    </v-list-item-icon>
+                                </v-list-item>
+                            </v-list-item-group>
+                        </v-list>
+                        <v-pagination :length="4" v-model="page" circle></v-pagination>
+                    </v-card-text>
+                </v-card>
+            </v-scroll-x-transition>
+        </v-row>
+        <FileUploadModal :modal="modal"></FileUploadModal>
+    </v-container>
+
 </template>
 
 <script>
-import VuetifyAudio from 'vuetify-audio';
-import FileUploadModal from './FileUploadModal';
+    import VuetifyAudio from 'vuetify-audio';
+    import FileUploadModal from './FileUploadModal';
 
-export default {
-    components: {
-        'v-audio': VuetifyAudio,
-        FileUploadModal
-    },
-    data: () => ({
-        modal: false,
-        page: 3,
-        item: null,
-        items: [
-            { text: 'Real-Time', icon: 'mdi-delete_outline' },
-            { text: 'Audience', icon: 'mdi-account' },
-            { text: 'Conversions', icon: 'mid-article' },
-        ]
-    })
-}
+    export default {
+        components: {
+            'v-audio': VuetifyAudio,
+            FileUploadModal
+        },
+        data: () => ({
+            modal: false,
+            page: 3,
+            item: null,
+            items: [
+                {text: 'Real-Time', icon: 'mdi-delete_outline'},
+                {text: 'Audience', icon: 'mdi-account'},
+                {text: 'Conversions', icon: 'mid-article'},
+            ]
+        })
+    }
 </script>
 
 <style>
     .container {
         background-image: url("https://file.mk.co.kr/mkde_7/N0/2019/09/20190911_4227439_1568163144.jpeg");
-        background-repeat : no-repeat;
-		background-size : cover;
+        background-repeat: no-repeat;
+        background-size: cover;
     }
 </style>
